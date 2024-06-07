dc15685d49c098e52fa800eb2cb82bb5cdc4a01e
JeisonPezoa <133894443+JeisonPezoa@users.noreply.github.com>
2024-06-07 15:14:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    cargarEstadoMenus();
+
+    const menuBoxes = document.querySelectorAll('.menu-box');
+    menuBoxes.forEach(menuBox => {
+        const countElement = menuBox.querySelector('.count');
+        const menuId = menuBox.id;
+        const count = localStorage.getItem(menuId);
+        if (count !== null) {
+            countElement.innerText = count;
+        }
+    });
+});
+
 function solicitarMenu(menuId) {
     const menuBox = document.getElementById(menuId);
     const countElement = menuBox.querySelector('.count');
@@ -5,6 +19,7 @@ function solicitarMenu(menuId) {
     if (count > 0) {
         count--;
         countElement.innerText = count;
+        localStorage.setItem(menuId, count);
         mostrarIngredientes(menuId); 
         setTimeout(() => {
             window.location.href = "imprimir.html?menu=" + menuId;
@@ -15,16 +30,25 @@ function solicitarMenu(menuId) {
 }
 
 function mostrarIngredientes(menuId) {
-   
     let ingredientes = {
         menu1: "Ingredientes del menú 1: Ingrediente 1, Ingrediente 2, Ingrediente 3",
         menu2: "Ingredientes del menú 2: Ingrediente A, Ingrediente B, Ingrediente C",
-       
+        menu3: "Ingredientes del menú 3: Ingrediente X, Ingrediente Y, Ingrediente Z"
     };
 
-    
     let descripcion = ingredientes[menuId];
-
-  
     alert(descripcion);
 }
+
+function cargarEstadoMenus() {
+    const menuBoxes = document.querySelectorAll('.menu-box');
+    menuBoxes.forEach(menuBox => {
+        const countElement = menuBox.querySelector('.count');
+        const menuId = menuBox.id;
+        const count = localStorage.getItem(menuId);
+        if (count !== null) {
+            countElement.innerText = count;
+        }
+    });
+}
+
